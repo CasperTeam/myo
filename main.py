76391140e41636f06f161a3e3ada79f8db214a22
SaTyEndRa <84174959+im-Satyendra@users.noreply.github.com>
2023-09-02 12:22:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client
 
-bot_token = "5067445854:AAFe_Em5hNWTQbaDq5kFfWNsR9JPe5M6RJc"
+bot_token = "5067445854:b"
 api_id = 2171111
 api_hash = "fd7acd07303760c52dcc0ed8b2f73086"
 
